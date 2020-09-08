7779bcbf09d220aa7b5fe8fb89ec9046433314ba
saveliy <cavacava1999@mail.ru>
2020-09-08 14:40:17 +0300

    Работающий метод прогонки и тест к нему

--- src/main/java/methods/SweepMethod.java
+++ src/main/java/methods/SweepMethod.java
@@ -56,4 +56,30 @@ public class SweepMethod {
         return x;
     }
 
+    public static double[] sweepMethod(double[] a, double[] b, double[] c, double[] d) throws NonSquareArray {
+        int n = a.length;
+        if (b.length != n ||c.length != n ||d.length != n ) {
+            throw new NonSquareArray("Массивы не соответсвуют!");
+        }
+        double[] answer = new double[n];
+        double[] aa = new double[n];
+        double[] bb = new double[n];
+
+        aa[0] = -c[0]/b[0];
+        bb[0] = d[0]/b[0];
+
+        for (int i = 1; i <= n-1; i++){
+            aa[i] = -c[i]/( b[i]+a[i]*aa[i-1]);
+            bb[i] = (d[i] - a[i]*bb[i-1])/(b[i]+a[i]*aa[i-1]);
+        }
+
+        answer[n-1] = bb[n-1];
+
+        for (int i = n-2; i>=0; i--){
+            answer[i] = aa[i]*answer[i+1] + bb[i];
+        }
+
+        return answer;
+    }
+
 }
--- src/test/java/TestForSweepMethod.java
+++ src/test/java/TestForSweepMethod.java
@@ -22,17 +22,21 @@ public class TestForSweepMethod {
 
     @Test
     public void testForSweepMethod2() throws NonSquareArray {
-        double[] A = {0,2,1,1}; // диагональ под главной
-        double[] B = {1,3,5,2}; // диагональ главная
-        double[] C = {2,1,6,0}; // диагональ над главной
-        double[] D = {1,2,3,4}; // правая часть
-        double[] x = SweepMethod.sweepMethod(A.length, A, B, C, D);
+        double[] A = {0,-3,-5,-6,-5}; // диагональ под главной
+        double[] B = {2,8,12,18,10}; // диагональ главная
+        double[] C = {-1,-1,2,-4,0}; // диагональ над главной
+        double[] D = {-25,72,-69,-156,20}; // правая часть
 
-        Assert.assertEquals(4, x.length);
-        Assert.assertEquals(7, x[0], 0.0001);
-        Assert.assertEquals(-3, x[1], 0.0001);
-        Assert.assertEquals(-3, x[2], 0.0001);
-        Assert.assertEquals(7.0/2, x[3], 0.0001);
+        double[] x = SweepMethod.sweepMethod( A, B, C, D);
+
+        System.out.println(Arrays.toString(x));
+
+        Assert.assertEquals(5, x.length);
+        Assert.assertEquals(-10, x[0], 0.0001);
+        Assert.assertEquals(5, x[1], 0.0001);
+        Assert.assertEquals(-2, x[2], 0.0001);
+        Assert.assertEquals(-10, x[3], 0.0001);
+        Assert.assertEquals(-3, x[4], 0.0001);
     }
 
     @Test(expected = NonSquareArray.class)
